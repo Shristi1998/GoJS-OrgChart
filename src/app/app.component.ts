7be945511fdfd3181a34b63af06ebe23e45c1676
Shristi Katyayani <shristik@vmware.com>
2020-12-10 22:53:42 +0530

    Added basic tree model and form for re-arranging org hierarchy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as go from 'gojs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'GoJS-OrgChart';
+
+  public selectedNode = null;
+
+  public model: go.TreeModel = new go.TreeModel(
+    [
+      { 'key': 1, 'name': 'Jaya', 'title': 'CEO' },
+      { 'key': 2, 'name': 'Avinash K', 'title': 'VP Sales', 'parent': 1 },
+      { 'key': 3, 'name': 'H. Mohan', 'title': 'Sales', 'parent': 2 },
+      { 'key': 4, 'name': 'Geeta Kumari', 'title': 'VP Engineering', 'parent': 1 },
+      { 'key': 5, 'name': 'Yashi Gupta', 'title': 'Manufacturing', 'parent': 4 },
+      { 'key': 6, 'name': 'Shubha M', 'title': 'Marketing', 'parent': 2 },
+      { 'key': 7, 'name': 'Dobil H', 'title': 'Sales Asst', 'parent': 3 },
+      { 'key': 8, 'name': 'Harshika Manak', 'title': 'Asst Project Manager', 'parent': 5 },
+      { 'key': 9, 'name': 'Surpiya Bhavsar', 'title': 'Team Lead', 'parent': 6 },
+      { 'key': 10, 'name': 'Mahmood', 'title': 'Engineering', 'parent': 4 },
+      { 'key': 11, 'name': 'Ali Ansari', 'title': 'Production', 'parent': 5 },
+      { 'key': 12, 'name': 'Ben K', 'title': 'Software Testing', 'parent': 10 },
+    ]
+  );
+
+  public setSelectedNode(node) {
+    this.selectedNode = node;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { GojsAngularModule } from 'gojs-angular';
 import { AppComponent } from './app.component';
+import { DiagramComponent } from './diagram/diagram.component';
+import { InspectorComponent } from './inspector/inspector.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiagramComponent,
+    InspectorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    GojsAngularModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
